e9e4f9a7f1b5a995dba04949bea8790cd14e4756
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-07-04 18:33:40 -0300

    :sparkles: Controller
    metodos Crud do controller implementados, e service iniciado

--- src/main/java/com/trybe/acc/java/minhasseries/controller/SerieController.java
+++ src/main/java/com/trybe/acc/java/minhasseries/controller/SerieController.java
@@ -1,12 +1,24 @@
 package com.trybe.acc.java.minhasseries.controller;
 
+import com.trybe.acc.java.minhasseries.exception.DataErrorException;
+import com.trybe.acc.java.minhasseries.exception.NotFoundException;
+import com.trybe.acc.java.minhasseries.model.Episodio;
 import com.trybe.acc.java.minhasseries.model.Serie;
 import com.trybe.acc.java.minhasseries.service.SerieService;
+import com.trybe.acc.java.minhasseries.service.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping(value = "/")
@@ -15,9 +27,58 @@ public class SerieController {
   @Autowired
   private SerieService service;
 
-  @GetMapping("/series")
-  public List<Serie> getSeries() {
-    return service.findAll();
+  @PostMapping("/series")
+  public ResponseEntity<Serie> criar(@RequestBody Serie serie) {
+    Serie serieSalva = service.create(serie);
+    return ResponseEntity.status(HttpStatus.OK).body(serieSalva);
   }
+
+  @GetMapping("/series")
+  public ResponseEntity<List<Serie>> listarTodos() {
+    List<Serie> series = service.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(series);
+  }
+
+  @DeleteMapping("/series/{serie_id}")
+  public ResponseEntity<Serie> deletar(
+      @PathVariable("serie_id") Integer serieId) throws NotFoundException {
+    service.delete(serieId);
+    return ResponseEntity.ok().build();
+  }
+
+  @CircuitBreaker(name = "episodios", fallbackMethod = "fallback")
+  @PostMapping("/series/{serie_id}/episodios")
+  public ResponseEntity<Serie> adicionarEpisodio(@RequestBody Episodio episodio,
+      @PathVariable("serie_id") Integer serieId) {
+    Serie serieAtualizada = service.addEpisode(serieId, episodio);
+    return ResponseEntity.status(HttpStatus.OK).body(serieAtualizada);
+  }
+
+  /**
+   * adicionarEpisodio fallback.
+   */
+  public ResponseEntity<DataErrorException> fallback(
+      @RequestBody Episodio episodio, @PathVariable("serie_id") Integer serieId,
+      Exception e) {
+    if (e.getClass() == NotFoundException.class) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+          .body(new DataErrorException(e.getMessage()));
+    }
+    return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+        .body(new DataErrorException("Serviço temporariamente indisponível"));
+  }
+
+  @GetMapping("/series/{serie_id}/episodios")
+  public ResponseEntity<List<Episodio>> listarEpisodios(
+      @PathVariable("serie_id") Integer serieId) {
+    List<Episodio> serieAtualizada = service.listEpisodes(serieId);
+    return ResponseEntity.status(HttpStatus.OK).body(serieAtualizada);
+  }
+
+  /*
+   * @GetMapping("/series/tempo") public ResponseEntity<Tempo> tempoSeries() {
+   * Tempo serieAtualizada = service.tempoSeries(); return
+   * ResponseEntity.status(HttpStatus.OK).body(serieAtualizada); }
+   */
 
 }
--- src/main/java/com/trybe/acc/java/minhasseries/service/SerieService.java
+++ src/main/java/com/trybe/acc/java/minhasseries/service/SerieService.java
@@ -1,5 +1,9 @@
 package com.trybe.acc.java.minhasseries.service;
 
+import com.trybe.acc.java.minhasseries.controller.Tempo;
+import com.trybe.acc.java.minhasseries.exception.AlreadyExistsException;
+import com.trybe.acc.java.minhasseries.exception.NotFoundException;
+import com.trybe.acc.java.minhasseries.model.Episodio;
 import com.trybe.acc.java.minhasseries.model.Serie;
 import com.trybe.acc.java.minhasseries.repository.SerieRepository;
 import java.util.List;
@@ -11,8 +15,41 @@ public class SerieService {
   @Autowired
   private SerieRepository repo;
 
+  public Serie create(Serie serieNova) {
+    if (repo.existsByNome(serieNova.getNome())) {
+      throw new AlreadyExistsException("Série Existente");
+    }
+    return repo.save(serieNova);
+  }
+
   public List<Serie> findAll() {
     return repo.findAll();
+  }
+
+  public void delete(Integer serieId) {
+    repo.findById(serieId)
+        .orElseThrow(() -> new NotFoundException("Serie não encontrada"));
+    repo.deleteById(serieId);
+  }
+
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  public Serie addEpisode(Integer serieId, Episodio episodio) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public List<Episodio> listEpisodes(Integer serieId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Tempo tempoSeries() {
+    // TODO Auto-generated method stub
+    return null;
   }
 
 }
